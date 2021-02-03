eac958e9f12d276a956d2a55ea81d4cd5ab5b9c7
katsutakeiichi <renoir5d@gmail.com>
2021-02-04 02:06:59 +0900

    add some environmental variables

--- cdk-static-cite-deploy/bin/cdk-static-cite-deploy.ts
+++ cdk-static-cite-deploy/bin/cdk-static-cite-deploy.ts
@@ -3,5 +3,10 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkStaticCiteDeployStack } from '../lib/cdk-static-cite-deploy-stack';
 
+const awsAccountID = process.env.AWS_ACCOUNT_ID || '';
+const awsRegion = process.env.AWS_DEFAULT_REGION || 'ap-northeast-1';
+const deployEnv = process.env.DEPLOY_ENV || 'staging';
+const suffix = deployEnv !== 'production' ? '-' + deployEnv : '';
+
 const app = new cdk.App();
-new CdkStaticCiteDeployStack(app, 'CdkStaticCiteDeployStack');
+new CdkStaticCiteDeployStack(app, `CdkStaticCiteDeployStack${suffix}`, { env: { account: awsAccountID, region: awsRegion}});
